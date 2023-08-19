10d1191f48d20280cfffcef8ef9259842c875f33
Ashanti Logan <Ashantilogan2@gmail.com>
2023-08-19 14:13:44 -0400

    commit tree 984543e45b39b34864b478363e72dea1600ce4a6

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+   cats.push(name)
+}
+
+console.log(cats)
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+console.log(cats)
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}
+function appendCat(name) {
+    const allCats = [...cats, name]
+return allCats
+}
+function prependCat(name) {
+    const allCats = [name, ...cats]
+return allCats
+}
+
+function removeLastCat(name) {
+    const allCats = [...cats]
+    allCats.pop(name)
+  return allCats  
+}
+
+function removeFirstCat(name) {
+    const allCats = [...cats]
+    allCats.shift(name)
+    return allCats
+}
